3ef555608c5dfa1768636a0d443feca60bea9552
Jin-Xu <adam@lostwire.net>
2019-11-11 11:16:06 +0000

    Adding __init__.py file

---
+++ pyced/__init__.py
@@ -0,0 +1,19 @@
+from pyced.aggregate import AggregateRoot, Event
+from pyced.decorators import unpack_event_data, expect_new_aggregate
+from pyced.store import init as store_init
+
+class Command(object):
+    def __init__(self, name, data, headers):
+        self._data = { 'name': name, 'data': data, 'headers': headers }
+
+    @property
+    def name(self):
+        return self._data['name']
+
+    @property
+    def data(self):
+        return self._data['data']
+
+    @property
+    def headers(self):
+        return self._data['headers']
