a423da49a907c99d650e19ca77eabea8cdcfe3f6
Jin-Xu <adam@lostwire.net>
2019-11-19 17:13:46 +0100

    Adding register

--- pyced/consumer.py
+++ pyced/consumer.py
@@ -5,7 +5,8 @@ import pyced.store
 def init(store_url):
     loop = asyncio.get_event_loop()
     store = pyced.store.init(store_url)
-    loop.run_until_complete(store.login('89f588b4-327e-44af-9cf1-6e6ba8e38226'))
+    loop.run_until_complete(store.register('username'))
+    loop.run_until_complete(store.login('username'))
     return Server(store, loop)
 
 class Server(object):
@@ -13,18 +14,10 @@ class Server(object):
         self._store = store
         self._loop = loop
         self._jar = {}
-    def register(self, name, handler):
+    def subscribe(self, name, handler):
         self._jar[name] = handler
         self._loop.run_until_complete(self._store.subscribe(name))
     async def consume(self, event):
         print(event.name, event.data)
     def run(self):
         self._store.consume_sync(self.consume)
-
-async def process(event):
-    print(event.name, event.data)
-
-s = init('http://estore-www:8080')
-s.register('Project',  process)
-s.register('project',  process)
-s.run()
