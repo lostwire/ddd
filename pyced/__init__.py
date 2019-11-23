f5a19323fe3b099eea5f7d316e88335be28de7ec
Jin-Xu <adam@lostwire.net>
2019-11-23 16:02:47 +0100

    Adding store_init

--- pyced/__init__.py
+++ pyced/__init__.py
@@ -1,2 +1,4 @@
-from pyced.saga.server import init
+from pyced.ddd import Saga, Event, AggregateRoot
 from pyced.store import init as store_init
+from pyced.decorators import unpack_event_data, expect_new_aggregate
+from pyced.saga.server import init
