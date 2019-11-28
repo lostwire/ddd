d188586405d78b13e3757b52ae81452af17a0e5d
Jin-Xu <adam@lostwire.net>
2019-11-28 15:17:30 +0100

    Sorting out the query server

--- pyced/query.py
+++ pyced/query.py
@@ -5,44 +5,23 @@ import aiohttp.web
 
 logger = logging.getLogger(__name__)
 
-def init(loop=None, **args):
-    if not loop:
-        loop = asyncio.get_event_loop()
-    return Server(aiohttp.web.Application(loop=loop), args)
+def init(**args):
+    return Server(aiohttp.web.Application(), args)
 
-async def json_output(f):
-    @functools.wraps(f)
-    def wrapper(*args, **kwargs):
-        return aiohttp.web.json_response(f(*args, **kwargs))
-    return wrapper
+async def json_output(f, *args, **kwargs):
+    return aiohttp.web.json_response(await f(*args, **kwargs))
 
 class Server(object):
     def __init__(self, app, args):
         self._app = app
         self._args = args
-
     def register(self, path, handler):
         logger.info("Registering handler for %s", path)
         self._app.router.add_get(path, functools.partial(json_output, handler))
-
+    def add_on_startup(self, callback):
+        self._app.on_startup.append(callback)
+    def add_on_cleanup(self, callback):
+        self._app.on_cleanup.append(callback)
     def run(self):
         logger.info("Starting server")
         aiohttp.web.run_app(self._app, **self._args)
-
-class App(object):
-    def __init__(self, store, loop):
-        self._jar = jar
-        self._loop = loop
-        self._store = store
-
-    async def on_event(self, event):
-        aggregate, method = event.name.split('.')
-        logger.info("Event arrived %s", event)
-        try:
-            if aggregate in self._jar:
-                await getattr(self._jar[aggregate], method)(event)
-        except Exception as e:
-            logger.error(str(e))
-
-    def run(self):
-        self._loop.run_until_complete(self._store.consume(self.on_event))
