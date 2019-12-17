4ac84970d7a380dc35921fee4ea3b7d1996d80b9
Jin-Xu <adam@lostwire.net>
2019-12-17 18:26:07 +0100

    Renaming exceptions to exc

--- pyced/decorators.py
+++ pyced/decorators.py
@@ -1,6 +1,6 @@
 import functools
 
-import pyced.exceptions
+import pyced.exc
 
 def unpack_event_data(callback):
     @functools.wraps(callback)
@@ -12,6 +12,6 @@ def expect_new_aggregate(callback):
     @functools.wraps(callback)
     def wrapper(obj, *args, **kwargs):
         if obj.version:
-            raise pyced.exceptions.AggregateAlreadyInitialized()
+            raise pyced.exc.AggregateAlreadyInitialized()
         return callback(obj, *args, **kwargs)
     return wrapper
